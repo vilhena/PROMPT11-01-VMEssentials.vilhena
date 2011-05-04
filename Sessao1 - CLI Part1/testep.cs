35bf3da5be86b8f0f89de1befa96e5c1ecd55d95
Goncalo Vilhena <vilhena@gmail.com>
2011-05-04 21:20:16 +0100

    parte 2 resolvida

--- Sessao1 - CLI Part1/testep.cs
+++ Sessao1 - CLI Part1/testep.cs
@@ -5,7 +5,15 @@ namespace Sessao1
     {
         static void Main(string[] args)
         {
-          
+            Ponto p1 = new Ponto(1, 5);
+            Ponto p2 = new Ponto(5, 6);
+            System.Console.Out.WriteLine(p1.ToString());
+            System.Console.Out.WriteLine(p2.ToString());
+            System.Console.Out.WriteLine(p1.Distance(p2));
+
+            Ponto p3 = new Ponto(1, 4);
+            Ponto p4 = new Ponto(1, 4);
+            System.Console.Out.WriteLine(p3.CompareTo(p4));
         }
     }
 }
