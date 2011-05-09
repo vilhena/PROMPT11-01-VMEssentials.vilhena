43c9a2b0d0ab82125f40519b5ea8c4105048457e
Goncalo Vilhena <vilhena@gmail.com>
2011-05-09 20:44:08 +0100

    project to dynamic add a method

---
+++ Sessao1 - CLI Part1/ProjectTest/JITRules/JITRules/JITBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace JITRules
+{
+    class JITBuilder
+    {
+        public static void patch(string dll, string method)
+        {
+            //AssemblyDefinition targetasmdef =
+            //     AssemblyFactory.GetAssembly("ReflexilDemo.exe");
+            //MethodDefinition computedefinition =
+            //    GetMethodDefinition(targetasmdef, "ReflexilDemo.DemoForm",
+            //    "ComputeAndDisplay", 2);
+
+            //CilWorker worker = computedefinition.Body.CilWorker;
+            //Instruction insld =
+            //    worker.Create(OpCodes.Ldstr, "The result is!");
+            //worker.InsertBefore(
+            //    computedefinition.Body.Instructions[11], insld);
+
+            //Instruction insshow = computedefinition.Body.Instructions[12];
+            //MethodReference oldshowref = insshow.Operand as MethodReference;
+
+            //AssemblyNameReference scope =
+            //    oldshowref.DeclaringType.Scope as AssemblyNameReference;
+            //DefaultAssemblyResolver resolver = new DefaultAssemblyResolver();
+            //AssemblyDefinition sourceasmdef = resolver.Resolve(scope);
+            //MethodDefinition newshowref = GetMethodDefinition(sourceasmdef,
+            //    "System.Windows.Forms.MessageBox", "Show", 2);
+
+            //insshow.Operand =
+            //    computedefinition.DeclaringType.Module.Import(newshowref);
+            //AssemblyFactory.SaveAssembly(targetasmdef,
+            //    "Reflexil.Manualy.Patched.exe");
+
+            AppDomain currentDomain = Thread.GetDomain();
+
+            AssemblyName myAsmName = Assembly.;
+            myAsmName.Name = "MyAssembly";
+
+
+
+
+ 
+           
+        }
+    }
+}
---
+++ Sessao1 - CLI Part1/ProjectTest/JITRules/Model/Airplane.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Airplane
+    {
+        private int p;
+        private string p_2;
+        private int p_3;
+        private int p_4;
+        private int p_5;
+
+        public int Seats { get; set; }
+        public string Name { get; set; }
+        public Tuple<int,int,int> Position { get; set; }
+
+        public Airplane(int seats, string name, int x, int y, int z)
+        {
+            this.Seats = seats;
+            this.Name = name;
+            this.Position = new Tuple<int, int, int>(x, y, z);
+        }
+
+
+        public int Altitude()
+        {
+            return this.Position.Item3;
+        }
+
+        public string Flight(string destination)
+        {
+            return "Goa";
+        }
+    }
+}
