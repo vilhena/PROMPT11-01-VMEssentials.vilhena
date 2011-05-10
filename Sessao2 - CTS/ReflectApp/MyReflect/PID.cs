c36faa3e8e6ac6983a3dad237449940e4b816638
Goncalo Vilhena <vilhena@gmail.com>
2011-05-10 23:08:14 +0100

    aula2

---
+++ Sessao2 - CTS/ReflectApp/MyReflect/PID.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MyReflect
+{
+
+    public class ObjectComparer : IEqualityComparer<object>
+    {
+        public int GetHashCode(object bx)
+        {
+            if(bx.GetType() == typeof(DirectoryInfo))
+                return ((DirectoryInfo) bx).FullName.GetHashCode();
+            else
+                return bx.GetHashCode();
+        }
+
+        public bool Equals(object b1, object b2)
+        {
+            if (b1.GetType() == typeof(DirectoryInfo) && b2.GetType() == typeof(DirectoryInfo))
+                return ((DirectoryInfo)b1).FullName == ((DirectoryInfo)b2).FullName;
+            else
+                return b1.Equals(b2);
+        }
+    }
+
+
+    public class PID
+    {
+        public static Dictionary<object, string> processed = new Dictionary<object, string>(new ObjectComparer());
+
+        public static void Inspect(object obj)
+        {
+
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                var value = property.GetValue(obj, new object[0]);
+                Console.WriteLine("{0} - {1}", property.Name, value == null ? String.Empty : value);
+            }
+        }
+
+        public static void InspectToFile(object obj)
+        {
+            var filename = obj.GetType().Name;
+
+            var sbuild = new StreamWriter(filename + ".html");
+
+            sbuild.WriteLine("<html>");
+            sbuild.WriteLine("<body>");
+            sbuild.WriteLine(@"<table border=1>");
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                sbuild.WriteLine("<tr>");
+
+                var value = property.GetValue(obj, new object[0]);
+                sbuild.WriteLine("<td>{0}</td><td>{1}</td>", property.Name, value == null ? String.Empty : value);
+
+                sbuild.WriteLine("</tr>");
+            }
+            sbuild.WriteLine("</table>");
+            sbuild.WriteLine("</body>");
+            sbuild.WriteLine("</html>");
+
+            sbuild.Close();
+        }
+
+
+        public static string Seek(object obj, string path)
+        {
+            var filename = path + "_" + obj.GetType().Name;
+
+            processed.Add(obj, filename);
+
+            var sbuild = new StreamWriter(@".\teste\" + filename + ".html");
+
+            sbuild.WriteLine("<html>");
+            sbuild.WriteLine("<body>");
+            sbuild.WriteLine("<h1>{0}<h1/>",obj.GetType().Name);
+            sbuild.WriteLine(@"<table border=1>");
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                if (property.GetIndexParameters().Length == 0)
+                {
+
+                    sbuild.WriteLine("<tr>");
+
+                    var value = property.GetValue(obj, new object[0]);
+
+
+                    if (property.PropertyType.IsPrimitive || value == null || property.PropertyType == typeof(String))
+                    {
+
+                        sbuild.WriteLine("<td>{0}</td><td>{1}</td>", property.Name, value == null ? String.Empty : value);
+                    }
+                    else
+                    {
+                        if (!processed.ContainsKey(value))
+                        {
+                            string processedfilename = Seek(value, filename);
+                            sbuild.WriteLine("<td>{0}</td><td><a href='{1}.html'>{2}</a></td>", property.Name, processedfilename, value.ToString());
+                            
+                        }
+                        else
+                        {
+                            sbuild.WriteLine("<td>{0}</td><td><a href='{1}.html'>{2}</a></td>", property.Name, processed[value], value.ToString());
+                        }
+                    }
+
+                    sbuild.WriteLine("</tr>");
+                }
+            }
+            sbuild.WriteLine("</table>");
+            sbuild.WriteLine("</body>");
+            sbuild.WriteLine("</html>");
+
+            sbuild.Close();
+
+            return filename;
+        }
+    }
+}
---
+++ Sessao2 - CTS/ReflectApp/ReflectApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyReflect;
+using System.IO;
+
+namespace ReflectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new DirectoryInfo(@"c:\program files");
+
+            
+            PID.Inspect(test);
+            PID.InspectToFile(test);
+            PID.Seek(test, "");
+
+        }
+    }
+}
